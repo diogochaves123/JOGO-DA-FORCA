9e172f0dd4e67b2af6bb3ecdc29a7f2b8839f73b
Diogo Chaves <diogo.24chaves@gmail.com>
2022-06-15 19:54:54 -0300

    PROJETO DO JOGO DA FORCA

---
+++ funcoes.py
@@ -0,0 +1,83 @@
+from ast import Num
+import os, random
+from typing import List
+def limpar():
+    os.system("cls")
+
+def start ():
+    print('''  
+     __
+    |
+    |
+    |
+    |
+    |  ''')
+
+def erro1():
+    print('''
+     __
+    |    |
+    |   ( )
+    |
+    |
+    | ''')
+
+def erro2():
+    print('''
+     __
+    |    |
+    |   ( )
+    |    |
+    |    |
+    |
+    | ''')
+
+def erro3():
+    print('''
+     __
+    |    |    
+    |   ( )
+    |    |\
+    |    |
+    |
+    |    
+    | ''')
+
+def erro4():
+    print('''
+     __
+    |    |    
+    |   ( )
+    |   /|\
+    |    |
+    |   / 
+    |    
+    |
+    ''')
+
+def perdeu():
+
+    print('''
+     __
+    |    |    
+    |   ( )
+    |   /|\
+    |    |
+    |   / \
+    |    
+    | 
+        
+   ''')
+
+
+
+
+
+
+
+
+
+
+
+
+
